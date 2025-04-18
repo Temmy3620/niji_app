42858a903407a980b2f1de5590046f7ae67f9676
terasakimio <me512papiko512@gmail.com>
2025-04-18 22:24:40 +0700

    ネオポルテ追加

--- src/constants/groupsConfig.ts
+++ src/constants/groupsConfig.ts
@@ -17,6 +17,11 @@ export const GROUPS_CONFIG: { key: GroupKey; name: string; iconUrl: string }[] =
     iconUrl: 'https://yt3.ggpht.com/YxkHbTBTSYsQjMHOp269eQUReXvJsTfM9gTEeByvNDGl97N6fqatLGRMZnHx-RIA_oPikX-e=s240-c-k-c0x00ffffff-no-rj',
   },
   {
+    key: 'neoporte',
+    name: 'ネオポルテ',
+    iconUrl: 'https://yt3.ggpht.com/B8q4Y3dRcxGtVCjgiIZcrlwUE34caO8e8QVSm-yCmkTgLopCFfm6MiJmrhGzd3gjOZ0V0dXV5w=s88-c-k-c0x00ffffff-no-rj',
+  },
+  {
     key: 'aogiri',
     name: 'あおぎり高校',
     iconUrl: 'https://yt3.ggpht.com/dDxwoOXbmmqwlnt8MMon3wPQdy5pX-vt-x8Eyqf38Yk8Zi2CIbJDXpzkA2YxZ1yUeOUhkweqYg=s88-c-k-c0x00ffffff-no-rj',
