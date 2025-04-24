ea3409f0631d1da79ce2b081576d22ead31da527
terasakimio <me512papiko512@gmail.com>
2025-04-25 02:52:56 +0700

    スマホサイズに対応

--- src/components/AppViewTabs.tsx
+++ src/components/AppViewTabs.tsx
@@ -10,7 +10,7 @@ type Props = {
 export const AppViewTabs = ({ currentTab, onChange }: Props) => {
   return (
     <Tabs value={currentTab} onValueChange={(value) => onChange(value as 'current' | 'subscribers' | 'views')} className="w-full justify-center mb-2 mt-2 px-4 md:px-6">
-      <TabsList className="flex justify-center gap-2 mt-4 mb-10 md:mb-2 flex-wrap">
+      <TabsList className="flex justify-center gap-2 mt-4 mb-16 md:mb-2 flex-wrap">
         <TabsTrigger
           value="current"
           className={`
