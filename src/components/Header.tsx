502bcaaea866f07c2a9f9d9fabc92ba09ff2ef06
terasakimio <me512papiko512@gmail.com>
2025-04-27 19:52:43 +0700

    ヘッダースマホ対応

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@
 
 export default function Header() {
   return (
-    <header className="relative overflow-hidden flex items-center justify-between px-2 md:px-6 bg-[url('/header.png')] bg-no-repeat bg-left bg-cover">
+    <header className="relative overflow-hidden flex items-center justify-between px-2 md:px-6 bg-black sm:bg-[url('/header.png')] sm:bg-no-repeat sm:bg-left sm:bg-cover">
       <div className="flex items-center z-10">
         <img src="/titleLogo.png" alt="App Icon" width={210} height={210} />
         <div className="absolute bottom-1 right-2 text-[8px] text-white opacity-80 sm:text-xs">
