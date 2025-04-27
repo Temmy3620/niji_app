be4b8113e48182ee966de61d1c871a11394b60b7
terasakimio <me512papiko512@gmail.com>
2025-04-27 19:48:09 +0700

    もとに戻す

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@
 
 export default function Header() {
   return (
-    <header className="relative overflow-hidden flex items-center justify-between px-2 md:px-6 bg-[url('/header.png')] bg-no-repeat bg-right bg-center bg-[length:50%]">
+    <header className="relative overflow-hidden flex items-center justify-between px-2 md:px-6 bg-[url('/header.png')] bg-no-repeat bg-left bg-cover">
       <div className="flex items-center z-10">
         <img src="/titleLogo.png" alt="App Icon" width={210} height={210} />
         <div className="absolute bottom-1 right-2 text-[8px] text-white opacity-80 sm:text-xs">
