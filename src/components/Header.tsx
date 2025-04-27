300e0802fd5c659c566dd8db5aa2653dadfa7400
terasakimio <me512papiko512@gmail.com>
2025-04-27 19:01:15 +0700

    Imgeから変更

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,10 @@
 'use client';
-import Image from 'next/image';
 
 export default function Header() {
   return (
     <header className="relative overflow-hidden flex items-center justify-between px-2 md:px-6 bg-[url('/header.png')] bg-no-repeat bg-left bg-cover">
       <div className="flex items-center z-10">
-        <Image src="/titleLogo.png" alt="App Icon" width={210} height={210} />
+        <img src="/titleLogo.png" alt="App Icon" width={210} height={210} />
         <div className="absolute bottom-1 right-2 text-[8px] text-white opacity-80 sm:text-xs">
           Your journey into the Vtuber realm starts here.
         </div>
